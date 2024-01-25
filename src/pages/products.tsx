3476d4057ffebe0237490c01d46e9871485e3bca
DaniloCHGS <danilochagasbm@gmail.com>
2024-01-25 18:11:31 -0300

    Adjusting padding in responsive

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -3,7 +3,7 @@ import { ProductsList } from "../components/ProductsList";
 
 export default function Products() {
   return (
-    <div className="min-h-screen bg-zinc-700 px-2">
+    <div className="min-h-screen bg-zinc-700 md:px-2 px-4">
       <div className="max-w-screen-md mx-auto space-y-4 pt-10">
         <Filters />
         <ProductsList />
